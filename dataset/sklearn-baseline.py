d35b62f4f553ab13c1009fe435f20eda2992ab3c
Rui Peng <pengrui1993@gmail.com>
2017-04-28 16:49:02 -0400

    use pd.read_csv instead of np.genfromtxt

--- dataset/sklearn-baseline.py
+++ dataset/sklearn-baseline.py
@@ -1,11 +1,16 @@
 import sys
 import time
 import numpy as np
+import pandas as pd
 from sklearn import tree
 
 assert len(sys.argv) == 3, 'train_tsv, test_tsv'
-train = np.genfromtxt(sys.argv[1], delimiter='\t')
-test = np.genfromtxt(sys.argv[2], delimiter='\t')
+
+# train = np.genfromtxt(sys.argv[1], delimiter='\t')
+# test = np.genfromtxt(sys.argv[2], delimiter='\t')
+train = pd.read_csv(sys.argv[1], sep='\t', header=None).values
+test = pd.read_csv(sys.argv[2], sep='\t', header=None).values
+
 x_train, y_train = train[:,1:], train[:,0]
 print 'train shape', x_train.shape, y_train.shape
 # print 'train', x_train[0], y_train[0]

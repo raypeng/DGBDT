285421a9fb4a6b39a33ca83f2156c8453d45fbae
Rui Peng <pengrui1993@gmail.com>
2017-04-28 15:44:47 -0400

    update xgboost code to use single thread

--- dataset/xgboost-baseline.py
+++ dataset/xgboost-baseline.py
@@ -1,5 +1,6 @@
-import numpy as np
 import sys
+import time
+import numpy as np
 import xgboost as xgb
 
 assert len(sys.argv) == 3, 'train_tsv, test_tsv'
@@ -10,9 +11,11 @@ test = np.genfromtxt(sys.argv[2], delimiter='\t')
 dtrain = xgb.DMatrix(train[:,1:], label=train[:,0])
 dtest = xgb.DMatrix(test[:,1:], label=test[:,0])
 # specify parameters via map
-param = {'max_leaves':255, 'objective':'multi:softprob', 'num_class': 5}
+param = {'max_leaves':255, 'objective':'multi:softprob', 'num_class': 5, 'nthread': 1}
 num_round = 1 # should make it regular decision tree
+_t = time.time()
 bst = xgb.train(param, dtrain, num_boost_round=num_round)
+print 'traing elapsed time:', time.time() - _t
 # make prediction
 pred_prob = bst.predict(dtrain)
 pred = np.argmax(pred_prob, axis=1)
